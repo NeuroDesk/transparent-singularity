e76ce217d347742734bf2c3101fb80d61a98cdc1
Steffen Bollmann <stebo85@users.noreply.github.com>
2024-04-26 10:44:54 +1000

    / was missing after neurodesk

--- run_transparent_singularity.sh
+++ run_transparent_singularity.sh
@@ -133,7 +133,7 @@ else
    echo "Testing standard Nectar Object storage next: "
    if curl --output /dev/null --silent --head --fail "https://objectstorage.us-ashburn-1.oraclecloud.com/n/sd63xuke79z3/b/neurodesk/o/$container"; then
       echo "$container exists in the standard nectar object storage"
-      url="https://object-store.rc.nectar.org.au/v1/AUTH_dead991e1fa847e3afcca2d3a7041f5d/neurodesk"
+      url="https://object-store.rc.nectar.org.au/v1/AUTH_dead991e1fa847e3afcca2d3a7041f5d/neurodesk/"
    fi
 
    if [[ -v url ]]; then
