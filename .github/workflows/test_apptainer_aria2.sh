025f47bb7c8affde2c7de6e88584b3973b8c36ae
Steffen Bollmann <stebo85@users.noreply.github.com>
2025-06-11 13:10:17 +1000

    change to apptainer-suid

--- .github/workflows/test_apptainer_aria2.sh
+++ .github/workflows/test_apptainer_aria2.sh
@@ -5,6 +5,6 @@ sudo apt update
 sudo apt install -y software-properties-common
 sudo add-apt-repository -y ppa:apptainer/ppa
 sudo apt update
-sudo apt install -y apptainer
+sudo apt install -y apptainer-suid
 
 bash /home/runner/work/transparent-singularity/transparent-singularity/.github/workflows/test_transparent_singularity_download.sh
--- .github/workflows/test_apptainer_curl.sh
+++ .github/workflows/test_apptainer_curl.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt install -y software-properties-common
 sudo add-apt-repository -y ppa:apptainer/ppa
 sudo apt update
-sudo apt install -y apptainer
+sudo apt install -y apptainer-suid
 
 sudo apt-get remove aria2
 
