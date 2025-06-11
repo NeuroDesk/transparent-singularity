5653a89389cc0f8176f5877fde877e8505a93542
Steffen Bollmann <stebo85@users.noreply.github.com>
2025-06-11 11:29:02 +1000

    fix installation of singularity and apptainer

--- .github/workflows/test_singularity_aria2.sh
+++ .github/workflows/test_singularity_aria2.sh
@@ -7,12 +7,16 @@ set -e
 # sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
 
 # This is for 20.04 (singularity-container is not build for later versions in neuro debian project yet)
-wget -O- http://neuro.debian.net/lists/focal.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
-sudo apt-key adv --recv-keys --keyserver hkps://keyserver.ubuntu.com 0xA5D32F012649A5A9
+# wget -O- http://neuro.debian.net/lists/focal.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+# sudo apt-key adv --recv-keys --keyserver hkps://keyserver.ubuntu.com 0xA5D32F012649A5A9
 
 # This is for 22.04 (but no singularity-container package available)
 # wget -O- http://neuro.debian.net/lists/jammy.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
 # sudo apt-key adv --recv-keys --keyserver hkps://keyserver.ubuntu.com 0xA5D32F012649A5A9
+
+# This is for 24.04
+wget -O- http://neuro.debian.net/lists/noble.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+sudo apt-key adv --recv-keys --keyserver hkps://keyserver.ubuntu.com 0xA5D32F012649A5A9
 
 sudo apt-get update
 sudo apt-get install singularity-container 
